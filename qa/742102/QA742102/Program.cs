2a5ecc75ada4c38d78c36e74c71b469b0b7d8b1d
Matthijs van der Veer <subscriptions@vanderveer.io>
2022-02-19 06:41:40 +0100

    Added sample project.

---
+++ qa/742102/QA742102/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Device.Location;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace QA742102
+{
+    class Program
+    {
+        private static DeviceClient deviceClient;
+
+        static void Main(string[] args)
+        {
+            deviceClient = DeviceClient.CreateFromConnectionString("<YOUR DEVICE CONNECTION STRING>");
+
+            var watcher = new GeoCoordinateWatcher();
+            watcher.PositionChanged += WatcherOnPositionChangedAsync;
+            var started = watcher.TryStart(false, TimeSpan.FromMilliseconds(1000));
+            if (!started)
+            {
+                Console.WriteLine("Out of luck.");
+                return;
+            }
+
+            Console.WriteLine("Enter any key to quit.");
+            Console.ReadLine();
+        }
+
+        private static async void WatcherOnPositionChangedAsync(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            Console.WriteLine(e.Position.Location.ToString());
+            var json = JsonConvert.SerializeObject(e.Position);
+            var eventMessage = new Message(Encoding.UTF8.GetBytes(json))
+            {
+                ContentEncoding = Encoding.UTF8.ToString(),
+                ContentType = "application/json",
+            };
+
+            await deviceClient.SendEventAsync(eventMessage);
+        }
+    }
+}
